9fc2857faa57b384c02644f84bb1215c0e3ace91
yusufwandana <yusuf.wandana1@gmail.com>
2021-07-29 19:46:07 +0700

    no refresh on enter input - fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({handleSearchChange}) => {
         <div className="flex flex-col justify-center items-center w-screen">
             <img src={Logo} className="w-72" alt="logo" />
             <h1 className="text-center text-4xl mb-10">Search a crypto currency</h1>
-            <form>
+            <form onSubmit={e => e.preventDefault()}>
             <input type="text" className="text-black w-72 h-14 px-3 border-2 rounded-md" placeholder="Search here" onChange={handleSearchChange} />
             </form>
         </div>
